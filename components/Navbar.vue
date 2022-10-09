382bd61c20d54c9eeaa4995db7c921736ab74a88
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2022-10-09 21:48:52 +0530

    Add navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/logo.png" alt="Logo" />
+      <h1><NuxtLink to="/">Assignment Hero</NuxtLink></h1>
+      <div class="links">
+        <button>Logout</button>
+        <NuxtLink class="btn" to="/signup">Sign Up</NuxtLink>
+        <NuxtLink class="btn" to="/login">Login</NuxtLink>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: white;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links {
+  margin-left: auto;
+}
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+nav img {
+  max-height: 60px;
+}
+</style>
